77b9252df5d746e396ac358ce4958a2d920c5f24
Md Bulbul Hossen <info247.bulbul@gmail.com>
2016-01-31 01:50:35 +0600

    time change

--- app/src/main/java/com/bignerdranch/android/splash/App.java
+++ app/src/main/java/com/bignerdranch/android/splash/App.java
@@ -12,6 +12,6 @@ public class App extends Application {
         super.onCreate();
 
         // Don't do this! This is just so cold launches take some time
-        SystemClock.sleep(TimeUnit.SECONDS.toMillis(3));
+        SystemClock.sleep(TimeUnit.SECONDS.toMillis(5));
     }
 }
